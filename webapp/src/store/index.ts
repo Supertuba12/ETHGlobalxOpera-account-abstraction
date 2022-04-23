3e980a78bc1800095388e3de70c6ce051fc00d5b
Hampus Eriksson <hamer848@student.liu.se>
2022-04-23 10:16:43 +0200

    Move store.js basic implementation to TypeScript.
    The rest of the webapp project will be TypeScript, so we shouldn't
    have random pure js files.
    
    This commit moves the basic store implementation to a TypeScript
    environment.

---
+++ webapp/src/store/index.ts
@@ -0,0 +1,14 @@
+import { createStore, Store } from "vuex";
+
+export const store: Store<any> = createStore({
+  state() {
+    return {
+      count: 0,
+    };
+  },
+  mutations: {
+    increment(state) {
+      state.count++;
+    },
+  },
+});
